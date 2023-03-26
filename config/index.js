3e6810cd5cb551fea6abb7b3ce6abdeb66971e13
circlehi <128978650+circlehi@users.noreply.github.com>
2023-03-26 21:17:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx2b0187f401b39699",
+  appsecret: "a81079727f04791fb79058b4bc8b9797",
+  template_id: "vUawqLe8iYfuGuAPb3RfEINYeKJlzQW1dYfVYEOdupY",
+  touser: "okp_I6MBkF6VYZXhxlUTCjEvcVXk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "常德",
+  first_date: "2002-7-23",
 };
